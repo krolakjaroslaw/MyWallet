d06992dcac06a2a501e97e7b05481d7a5212beb0
jk <krolak.jaroslaw@gmail.com>
2021-02-24 18:23:25 +0100

    Charts view - example of chart by charts.js

---
+++ pages/charts.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="chart">
+    <canvas id="planet-chart" />
+  </div>
+</template>
+
+<script>
+import Chart from 'chart.js'
+import chartData from '../assets/chart-data'
+import json from '../assets/data.json'
+
+export default {
+  data () {
+    return {
+      chartData
+    }
+  },
+  mounted () {
+    const labels = json.main.map(el => new Date(el[0]))
+    const values = json.main.map(el => el[1])
+    this.createChart('planet-chart', this.chartData(labels, values))
+  },
+  methods: {
+    createChart (chartId, chartData) {
+      const ctx = document.getElementById(chartId)
+      const myChart = new Chart(ctx, {
+        type: chartData.type,
+        data: chartData.data,
+        options: chartData.options
+      })
+      console.log(myChart)
+    }
+  }
+}
+
+</script>
