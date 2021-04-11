1363dcfc2f860e73a2d0810835006c8c9ee11e4a
jk <krolak.jaroslaw@gmail.com>
2021-04-11 20:18:58 +0200

    Connection with backend:
    * getWallets
    * createWallet
    * deleteWallet
    * updateWalletName
    * getWallet (lack of screen with wallet-details)

--- plugins/backend.js
+++ plugins/backend.js
@@ -1,9 +1,13 @@
 import authorization from '~/lib/api/authorization'
+import wallets from '~/lib/api/wallets'
 
 export default ({ $axios, store }, inject) => {
   inject('backend', {
     authorization: {
       ...authorization($axios, store)
+    },
+    wallets: {
+      ...wallets($axios, store)
     }
   })
 }
